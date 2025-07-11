0c4eec707dce15b3c6466d6966cb5b1bf4399618
照坤 <s.yellow@foxmail.com>
2025-07-11 16:25:29 +0800

    chore: update .gitignore and .vscode settings to exclude mock compilation files; remove unused mock modules to streamline project structure and improve maintainability

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,62 +1,32 @@
+// src/mock/index.ts
 import Mock from 'mockjs'
 
-/**
- * @param {string} url
- * @returns {Object}
- */
-function param2Obj(url) {
+function param2Obj(url: string) {
   const search = decodeURIComponent(url.split('?')[1] || '').replace(/\+/g, ' ')
   return Object.fromEntries(search.split('&').map(item => item.split('=')))
 }
 
+// 自动导入所有 mock 模块
+const modules = import.meta.glob('./module/*.ts', { eager: true });
+export const mocks = Object.values(modules).flatMap((mod: any) => mod.default || mod);
 
-import auth from './module/auth'
-import dept from './module/dept'
-import dictData from './module/dict-data'
-import menu from './module/menu'
-import notifyMessage from './module/notify-message'
-import role from './module/role'
-import tenant from './module/tenant'
-import user from './module/user'
-import data from './module/data'
-
-export const mocks = [
-  ...auth,
-  ...menu,
-  ...dept,
-  ...dictData,
-  ...notifyMessage,
-  ...role,
-  ...tenant,
-  ...user,
-  ...data
-]
-
-// for front mock
-// please use it cautiously, it will redefine XMLHttpRequest,
-// which will cause many of your third-party libraries to be invalidated(like progress event).
 export function mockXHR() {
-  // mock patch
-  // https://github.com/nuysoft/Mock/issues/300
   Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
   Mock.XHR.prototype.send = function() {
     if (this.custom.xhr) {
       this.custom.xhr.withCredentials = this.withCredentials || false
-
       if (this.responseType) {
         this.custom.xhr.responseType = this.responseType
       }
     }
-    // eslint-disable-next-line prefer-rest-params
     this.proxy_send(...arguments)
   }
 
-  function XHR2ExpressReqWrap(respond) {
-    return function(options) {
+  function XHR2ExpressReqWrap(respond: any) {
+    return function(options: any) {
       let result = null
-      if (respond instanceof Function) {
+      if (typeof respond === 'function') {
         const { body, type, url } = options
-        // https://expressjs.com/en/4x/api.html#req
         result = respond({
           method: type,
           body: JSON.parse(body),
@@ -69,7 +39,7 @@ export function mockXHR() {
     }
   }
 
-  mocks.forEach(mock => {
+  mocks.forEach((mock: any) => {
     Mock.mock(new RegExp(mock.url), mock.type || 'get', XHR2ExpressReqWrap(mock.response))
   })
 }
